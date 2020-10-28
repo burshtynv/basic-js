2e195d8cef9ead3eeca16c08b80a9fe0be7ddc00
burshtynv <vanyaburshtyn@gmail.com>
2020-10-28 17:40:22 +0300

    solution task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,9 +3,9 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard) {
 
   let numberOfCounts = 0;
-  for (let i = 0; i < backyard.lemgth; i++);
-  for (let j = 0; j < backyard.lemgth; j++)
-  if(backyard[i][j] === '^^'){
+  for (let i = 0; i < backyard.length; i++)
+  for (let j = 0; j < backyard[i].length; j++)
+  if(backyard[i][j] === "^^"){
     numberOfCounts+=1;
   }
   return numberOfCounts;
